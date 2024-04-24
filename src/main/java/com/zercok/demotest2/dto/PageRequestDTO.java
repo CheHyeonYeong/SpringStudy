411a4f8f8e734294bd1623de4a1bba36f4a36cb7
unknown <gusdudco6@naver.com>
2024-04-24 11:20:15 +0900

    ✨ List paging 작업
    list paging 작업

--- src/main/java/com/zercok/demotest2/dto/PageRequestDTO.java
+++ src/main/java/com/zercok/demotest2/dto/PageRequestDTO.java
@@ -18,16 +18,25 @@ public class PageRequestDTO {
     private int page =1;
 
     @Builder.Default //default 값으로 썼다
-    @Min(value=10)
+    @Min(value = 10)
     @Max(value = 100)
     @Positive
     private int size = 10;
+
+    private String link; //페이지와 사이즈 정보를 같이 넘기기 위한 변수
 
     //맨 끝 페이지로 넣어감
     public int getSkip(){
         return (page-1)*size;
     }
 
-
-
+    public String getLink() {
+        if(link == null){
+            StringBuilder builder = new StringBuilder();
+            builder.append("page="+this.page);
+            builder.append("&size="+this.size);
+            link = builder.toString();
+        }
+        return link;
+    }
 }
